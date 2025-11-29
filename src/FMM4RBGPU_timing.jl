01ca7376c2eb3290f16560d8881ca5975896f4d4
mortezamsp <mortezamsp.2014@gmail.com>
2025-11-29 18:00:06 +0330

    bug fix 2

--- src/FMM4RBGPU_timing.jl
+++ src/FMM4RBGPU_timing.jl
@@ -11,8 +11,9 @@ include("utils/utils.jl")
 include("cluster_tree/cluster_tree.jl")
 include("fmm/fmm.jl")
 include("gpu_fmm/gpu_fmm.jl")
-include("update_particles_field.jl")
 
+# Import the complete implementation as a submodule
+include("update_particles_field.jl")
+using .FMM4RBGPUComplete: BruteForce, FMM, FMMGPU, update_particles_field!
 
 end
-
