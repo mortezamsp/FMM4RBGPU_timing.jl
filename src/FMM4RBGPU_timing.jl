6c1d98c6db98d1de39617e37d1f9b3d45c1835d5
mortezamsp <mortezamsp.2014@gmail.com>
2025-11-29 19:50:33 +0330

    bug fix 2

--- src/FMM4RBGPU_timing.jl
+++ src/FMM4RBGPU_timing.jl
@@ -6,14 +6,11 @@ using Dates
 
 export Particles, Beam
 
-include("particles.jl")
+include("particles.jl")  # This should come first
 include("utils/utils.jl")
 include("cluster_tree/cluster_tree.jl")
 include("fmm/fmm.jl")
 include("gpu_fmm/gpu_fmm.jl")
-
-# Import the complete implementation as a submodule
-include("update_particles_field.jl")
-using .FMM4RBGPUComplete: BruteForce, FMM, FMMGPU, update_particles_field!
+include("update_particles_field.jl")  # This uses Particles from particles.jl
 
 end
